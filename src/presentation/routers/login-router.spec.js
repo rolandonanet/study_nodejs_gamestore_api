dd3de760a25eb0db3dbf6517ed3ee52bc19864cd
Rodrigo Dantas <rodrigodantas.91@gmail.com.br>
2021-02-10 09:47:59 -0300

    implementing new sample test

--- src/presentation/routers/login-router.spec.js
+++ src/presentation/routers/login-router.spec.js
@@ -1,6 +1,6 @@
 class LoginRouter {
   route (httpRequest) {
-    if (!httpRequest.body.email) {
+    if (!httpRequest.body.email || !httpRequest.body.password) {
       return {
         statusCode: 400
       }
@@ -14,6 +14,20 @@ describe('login router', () => {
     const httpRequest = {
       body: {
         password: 'anyPassword'
+      }
+    }
+    const httpResponse = loginRouter.route(httpRequest)
+
+    expect(httpResponse.statusCode).toBe(400)
+  })
+})
+
+describe('login router', () => {
+  test('should return 400 if password was not provided', () => {
+    const loginRouter = new LoginRouter()
+    const httpRequest = {
+      body: {
+        email: 'any@email.com'
       }
     }
     const httpResponse = loginRouter.route(httpRequest)
